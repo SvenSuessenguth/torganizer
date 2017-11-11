cea896fc785a1e3bb592b175b8f92cff2e270465
SvenSuessenguth <familie.suessenguth@googlemail.com>
2017-11-11 13:34:26 +0100

    validation angedeutet

---
+++ validation/src/test/java/org/cc/torganizer/validation/ValidatePersonTest.java
@@ -0,0 +1,72 @@
+package org.cc.torganizer.validation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.cc.torganizer.core.entities.Person;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ValidatePersonTest {
+  
+  private static Validator validator;
+  
+  @BeforeClass
+  public static void beforeClass() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+  
+  @Test
+  public void testValidatePerson_firstNameTooShort() {
+    String tooShortFirstName = "";
+    Person p = new Person(tooShortFirstName, "ok");
+    
+    Set<ConstraintViolation<Person>> violations = validator.validate( p );
+    
+    // notBlank, min-size=1
+    assertThat(violations.size(), is(2));
+  }
+  
+  @Test
+  public void testValidatePerson_firstNameBlank() {
+    String tooShortFirstName = "   ";
+    Person p = new Person(tooShortFirstName, "ok");
+    
+    Set<ConstraintViolation<Person>> violations = validator.validate( p );
+    
+    // notBlank
+    assertThat(violations.size(), is(1));
+  }
+  
+  @Test
+  public void testValidatePerson_firstNameNull() {
+    String tooShortFirstName = null;
+    Person p = new Person(tooShortFirstName, "ok");
+    
+    Set<ConstraintViolation<Person>> violations = validator.validate( p );
+    
+    // notNull, notBlank
+    assertThat(violations.size(), is(2));
+  }
+  
+  @Test
+  public void testValidatePerson_firstNameTooLong() {
+    // 21 Zeichen
+    String tooShortFirstName = "012345678901234567890";
+    Person p = new Person(tooShortFirstName, "ok");
+    
+    Set<ConstraintViolation<Person>> violations = validator.validate( p );
+    
+    // max-size=20
+    assertThat(violations.size(), is(1));
+  }
+}
