e356ef362b731975090027ae0857f311f0f00abc
svens <familie.suessenguth@googlemail.com>
2017-11-20 08:41:28 +0100

    Warnung aus SonarQube beseitigt

--- model/src/main/java/org/cc/torganizer/core/entities/AbstractBaseEntity.java
+++ model/src/main/java/org/cc/torganizer/core/entities/AbstractBaseEntity.java
@@ -18,26 +18,4 @@ abstract class AbstractBaseEntity
   public boolean isTransient() {
     return getId() == null;
   }
-
-  /**
-   * {@inheritDoc} Gleichheit wird durch die DB-ID festgestellt.
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof BaseEntity)) {
-      return false;
-    }
-
-    BaseEntity other = (BaseEntity) obj;
-    if (this.getId() == null || other.getId() == null) {
-      return false;
-    }
-    return this.getId() != null && this.getId().equals(other.getId());
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public int hashCode() {
-    return this.getId() == null ? super.hashCode() : this.getId().hashCode();
-  }
 }
