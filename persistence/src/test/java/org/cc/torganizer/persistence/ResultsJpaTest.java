1b7d2cb85db0884d7a85b223966478f17713ffc5
SvenSuessenguth <familie.suessenguth@googlemail.com>
2017-11-22 17:55:17 +0100

    Test für Result hinzugefügt

--- persistence/src/test/java/org/cc/torganizer/persistence/ResultsJpaTest.java
+++ persistence/src/test/java/org/cc/torganizer/persistence/ResultsJpaTest.java
@@ -2,10 +2,12 @@ package org.cc.torganizer.persistence;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 import java.util.List;
 
 import org.cc.torganizer.core.entities.Result;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,5 +22,14 @@ public class ResultsJpaTest extends AbstractDbUnitJpaTest {
   public void testFindAll() {
     List<Result> results = entityManager.createNamedQuery("Result.findAll", Result.class).getResultList();
     assertThat(results, hasSize(3));
+  }
+  
+  @Test
+  public void testFindById() {
+    List<Result> results = entityManager.createNamedQuery("Result.findById", Result.class).setParameter("id", 3L).getResultList();
+    assertThat(results, hasSize(1));
+    Result r = results.get(0);
+    assertThat(r.isDraw(), is(true));
+    assertThat(r.getPosition(), is(3));
   }
 }
