642ad73fe326c136014c237a6d4dd82801dcd07c
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-03-26 18:30:52 +0200

    compile error

--- persistence/src/main/java/org/cc/torganizer/persistence/Repository.java
+++ persistence/src/main/java/org/cc/torganizer/persistence/Repository.java
@@ -5,5 +5,5 @@ public class Repository {
   protected static final int DEFAULT_MAX_RESULTS = 10;
   protected static final int DEFAULT_OFFSET = 0;
 
-  private Repository(){}
+  protected Repository(){}
 }
