e6b344d76d4e9525dadb4f1434e0aa2d0b1bec6d
svens <familie.suessenguth@googlemail.com>
2018-03-27 11:13:43 +0200

    remove entityManager from PlayersResource

--- persistence/src/main/java/org/cc/torganizer/persistence/PlayersRepository.java
+++ persistence/src/main/java/org/cc/torganizer/persistence/PlayersRepository.java
@@ -4,7 +4,6 @@ import org.cc.torganizer.core.entities.Player;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
