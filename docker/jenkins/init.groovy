52447fc07f2c5763143ddc058ec67b8ecd6d466f
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-04-21 10:42:11 +0200

    jenkins on docker

--- docker/jenkins/init.groovy
+++ docker/jenkins/init.groovy
@@ -22,32 +22,15 @@ instance.save()
 // Allow configuration of jenkins proxy host and proxy port
 // https://github.com/geerlingguy/ansible-role-jenkins/issues/28
 // ---------------------------------------------------------------------------------------------------------------------------------
-final String name = "proxy.system.local"
-final int port = 80
-final String userName = "techuser"
-final String password = "gF5!aL"
-final String noProxyHost = "*.system.local \n*.system-a.local \nlocalhost \n127.0.0.1"
+//final String name = "proxy.system.local"
+//final int port = 80
+//final String userName = "techuser"
+//final String password = "gF5!aL"
+//final String noProxyHost = "*.system.local \n*.system-a.local \nlocalhost \n127.0.0.1"
 
-final def pc = new hudson.ProxyConfiguration(name, port, userName, password, noProxyHost)
-instance.proxy = pc
-instance.save()
-
-// ---------------------------------------------------------------------------------------------------------------------------------
-// Install Plugins
-// https://github.com/coreos/jenkins-os/blob/master/init.groovy
-// ---------------------------------------------------------------------------------------------------------------------------------
-final List<String> REQUIRED_PLUGINS = [
-  "sonar",
-]
-if (Jenkins.instance.pluginManager.plugins.collect {
-        it.shortName
-    }.intersect(REQUIRED_PLUGINS).size() != REQUIRED_PLUGINS.size()) {
-    REQUIRED_PLUGINS.collect {
-        Jenkins.instance.updateCenter.getPlugin(it).deploy()
-    }.each {
-        it.get()
-    }
-}
+// final def pc = new hudson.ProxyConfiguration(name, port, userName, password, noProxyHost)
+// instance.proxy = pc
+// instance.save()
 
 // ---------------------------------------------------------------------------------------------------------------------------------
 // Disable Setup Wizard
