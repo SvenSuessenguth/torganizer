799919eafadbab8ac861f24aee4ed8f80000e41c
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-04-30 12:56:37 +0200

    show name of discipline on rounds page

--- webclient/src/main/webapp/resources/js/rounds.js
+++ webclient/src/main/webapp/resources/js/rounds.js
@@ -5,7 +5,30 @@ class Rounds {
   }
 
   onload() {
+    this.initSessionStorage(null);
+    this.initDisciplineName();
   }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  //
+  // initialize with values from sessionStorage
+  //
+  //--------------------------------------------------------------------------------------------------------------------
+  // initilize with default values only if nothing is already in sessionStorage
+  initSessionStorage(selectedDisciplineId){
+  }
+  initDisciplineName(){
+    let currentDisciplineId = sessionStorage.getItem('disciplines.current-discipline.id');
+    if(currentDisciplineId===null){return;}
+
+    disciplinesResource.readSingle(currentDisciplineId, this.initDisciplineNameResolve, this.initDisciplineNameReject);
+  }
+  initDisciplineNameResolve(discipline){
+    let name = discipline.name;
+    let dnElement = document.getElementById("disciplineName");
+    dnElement.innerHTML = name;
+  }
+  initDisciplineNameReject(json){ }
 }
 
 var rounds = new Rounds();
