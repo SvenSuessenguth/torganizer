fdd1cd7a9d14cc62b059f3b19b47af9985370ac4
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-06-16 12:52:16 +0200

    MatchRepository with test

---
+++ persistence/src/main/java/org/cc/torganizer/persistence/MatchesRepository.java
@@ -0,0 +1,69 @@
+package org.cc.torganizer.persistence;
+
+import org.cc.torganizer.core.entities.Match;
+import org.cc.torganizer.core.entities.Player;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Stateless
+public class MatchesRepository extends Repository{
+
+  public MatchesRepository() {
+  }
+
+  /**
+   * Constructor for testing.
+   */
+  public MatchesRepository(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  //
+  // Person CRUD
+  //
+  //--------------------------------------------------------------------------------------------------------------------
+  public Match create(Match match){
+    entityManager.persist(match);
+    // with no flush, the id is unknown
+    entityManager.flush();
+
+    return match;
+  }
+  public Match read(Long matchId) {
+    return entityManager.find(Match.class, matchId);
+  }
+
+  public List<Match> read(Integer offset, Integer maxResults){
+    offset = offset == null ? DEFAULT_OFFSET : offset;
+    maxResults = maxResults == null ? DEFAULT_MAX_RESULTS : maxResults;
+
+    TypedQuery<Match> namedQuery = entityManager.createNamedQuery("Match.findAll", Match.class);
+    namedQuery.setFirstResult(offset);
+    namedQuery.setMaxResults(maxResults);
+    return namedQuery.getResultList();
+  }
+
+  public Match update(Match match){
+    entityManager.merge(match);
+
+    return match;
+  }
+
+  public Match delete(Long matchId){
+    Match match = entityManager.find(Match.class, matchId);
+
+    entityManager.remove(match);
+
+    return match;
+  }
+
+  public long count(){
+    Query query = entityManager.createQuery("SELECT count(m) FROM Match m");
+    return (long) query.getSingleResult();
+  }
+}
--- persistence/src/test/java/org/cc/torganizer/persistence/MatchesJpaTest.java
+++ persistence/src/test/java/org/cc/torganizer/persistence/MatchesJpaTest.java
@@ -1,11 +1,29 @@
 package org.cc.torganizer.persistence;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class MatchesJpaTest extends AbstractDbUnitJpaTest {
+
+  private MatchesRepository repository;
 
   @BeforeEach
   public void before() throws Exception {
     super.initDatabase("test-data-matches.xml");
+
+    repository = new MatchesRepository(entityManager);
+  }
+
+  @Test
+  public void testCount(){
+    Long countExpected = 1L;
+    Long countActual = repository.count();
+
+    assertThat(countActual, is(countExpected));
   }
 }
