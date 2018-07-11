db5bc9024ee775c52056173b44e92dc38f3f8077
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-07-11 14:45:55 +0200

    default constructor

--- persistence/src/main/java/org/cc/torganizer/persistence/TournamentsRepository.java
+++ persistence/src/main/java/org/cc/torganizer/persistence/TournamentsRepository.java
@@ -18,6 +18,9 @@ import java.util.stream.Collectors;
 @Stateless
 public class TournamentsRepository extends Repository {
 
+    public TournamentsRepository() {
+    }
+
     /**
      * Constructor for testing purpose.
      *
