baa6bc9e40af806e8a17d2c0b5360617051d797b
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-09-23 07:14:06 +0200

    remove checkstyle warnings

--- rest/src/main/java/org/cc/torganizer/rest/TournamentsResource.java
+++ rest/src/main/java/org/cc/torganizer/rest/TournamentsResource.java
@@ -1,20 +1,42 @@
 package org.cc.torganizer.rest;
 
-import org.cc.torganizer.core.entities.*;
-import org.cc.torganizer.persistence.DisciplinesRepository;
-import org.cc.torganizer.persistence.TournamentsRepository;
-import org.cc.torganizer.rest.json.*;
-
+import java.util.List;
+import java.util.Set;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.validation.*;
-import javax.ws.rs.*;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.Set;
+
+import org.cc.torganizer.core.entities.Discipline;
+import org.cc.torganizer.core.entities.Opponent;
+import org.cc.torganizer.core.entities.OpponentType;
+import org.cc.torganizer.core.entities.OpponentTypeRestriction;
+import org.cc.torganizer.core.entities.Player;
+import org.cc.torganizer.core.entities.Squad;
+import org.cc.torganizer.core.entities.Tournament;
+import org.cc.torganizer.persistence.DisciplinesRepository;
+import org.cc.torganizer.persistence.TournamentsRepository;
+import org.cc.torganizer.rest.json.DisciplineJsonConverter;
+import org.cc.torganizer.rest.json.ModelJsonConverter;
+import org.cc.torganizer.rest.json.OpponentJsonConverterProvider;
+import org.cc.torganizer.rest.json.PlayerJsonConverter;
+import org.cc.torganizer.rest.json.SquadJsonConverter;
+import org.cc.torganizer.rest.json.TournamentJsonConverter;
 
 import static org.cc.torganizer.core.entities.OpponentType.PLAYER;
 import static org.cc.torganizer.core.entities.OpponentType.SQUAD;
@@ -26,178 +48,201 @@ import static org.cc.torganizer.core.entities.Restriction.Discriminator.OPPONENT
 @Consumes("application/json")
 public class TournamentsResource extends AbstractResource {
   @Inject
-  private TournamentsRepository tRepository;
+  private TournamentsRepository tournamentsRepo;
   @Inject
-  private DisciplinesRepository dRepository;
+  private DisciplinesRepository disciplineRepo;
 
   @Inject
-  private TournamentJsonConverter tConverter;
+  private TournamentJsonConverter tournamentConverter;
 
   @Inject
-  private DisciplineJsonConverter dConverter;
+  private DisciplineJsonConverter disciplineConverter;
 
   @Inject
-  private OpponentJsonConverterProvider ocProvider;
+  private OpponentJsonConverterProvider opponentConverterProvider;
 
 
   @POST
   public Response create(JsonObject jsonObject) {
-    Tournament tournament = tConverter.toModel(jsonObject, new Tournament());
+    Tournament tournament = tournamentConverter.toModel(jsonObject, new Tournament());
     // client can send '0' with a detached object exception as the result
     tournament.setId(null);
 
     validate(tournament);
 
-    Tournament savedTournament = tRepository.create(tournament);
-    return Response.ok(tConverter.toJsonObject(savedTournament)).build();
+    Tournament savedTournament = tournamentsRepo.create(tournament);
+    return Response.ok(tournamentConverter.toJsonObject(savedTournament)).build();
   }
 
   @GET
   @Path("{id}")
   public JsonObject readSingle(@PathParam("id") Long id) {
-    Tournament tournament = tRepository.read(id);
-    return tConverter.toJsonObject(tournament);
+    Tournament tournament = tournamentsRepo.read(id);
+    return tournamentConverter.toJsonObject(tournament);
   }
 
   @GET
-  public JsonArray readMultiple(@QueryParam("offset") Integer offset, @QueryParam("maxResults") Integer maxResults) {
-    List<Tournament> tournaments = tRepository.getTournaments(offset, maxResults);
+  public JsonArray readMultiple(@QueryParam("offset") Integer offset,
+                                @QueryParam("maxResults") Integer maxResults) {
+    List<Tournament> tournaments = tournamentsRepo.getTournaments(offset, maxResults);
 
-    return tConverter.toJsonArray(tournaments);
+    return tournamentConverter.toJsonArray(tournaments);
   }
 
   @PUT
   public JsonObject update(JsonObject jsonObject) {
     Long id = Long.valueOf(jsonObject.get("id").toString());
-    Tournament tournament = tRepository.read(id);
+    Tournament tournament = tournamentsRepo.read(id);
 
-    tournament = tConverter.toModel(jsonObject, tournament);
+    tournament = tournamentConverter.toModel(jsonObject, tournament);
     validate(tournament);
 
-    tRepository.update(tournament);
+    tournamentsRepo.update(tournament);
 
-    return tConverter.toJsonObject(tournament);
+    return tournamentConverter.toJsonObject(tournament);
   }
 
   @DELETE
   public JsonObject delete(JsonObject jsonObject) {
     Long id = Long.valueOf(jsonObject.get("id").toString());
-    Tournament tournament = tRepository.read(id);
+    Tournament tournament = tournamentsRepo.read(id);
 
-    tRepository.delete(tournament);
+    tournamentsRepo.delete(tournament);
 
-    return tConverter.toJsonObject(tournament);
+    return tournamentConverter.toJsonObject(tournament);
   }
 
   @GET
   @Path("/{id}/players")
-  public JsonArray players(@PathParam("id") Long tournamentId, @QueryParam("offset") Integer offset, @QueryParam("maxResults") Integer maxResults) {
-    List<Player> players = tRepository.getPlayersOrderedByLastName(tournamentId, offset, maxResults);
+  public JsonArray players(@PathParam("id") Long tournamentId, @QueryParam("offset") Integer offset,
+                           @QueryParam("maxResults") Integer maxResults) {
+    List<Player> players = tournamentsRepo.getPlayersOrderedByLastName(tournamentId, offset,
+        maxResults);
 
-    PlayerJsonConverter pConverter = (PlayerJsonConverter) ocProvider.getConverter(PLAYER);
-    return pConverter.toJsonArray(players);
+    PlayerJsonConverter playerConverter =
+        (PlayerJsonConverter) opponentConverterProvider.getConverter(PLAYER);
+
+    return playerConverter.toJsonArray(players);
   }
 
   /**
    * Getting all opponents, which can be assigned to a given discipline for the tournament.
+   *
    * @param tournamentId ID of the tournament
    * @param disciplineId ID of the discipline
-   * @param offset offset
-   * @param maxResults max results
+   * @param offset       offset
+   * @param maxResults   max results
    * @return JasonArray with the assignable opponents
    */
   @SuppressWarnings("unchecked")
   @GET
   @Path("/{id}/assignable-opponents")
-  public JsonArray getAssignableOpponents(@PathParam("id") Long tournamentId, @QueryParam("disciplineId") Long disciplineId,
-                                          @QueryParam("offset") Integer offset, @QueryParam("maxResults") Integer maxResults) {
-    Discipline discipline = dRepository.read(disciplineId);
-    List<Opponent> opponents = tRepository.getOpponentsForDiscipline(tournamentId, discipline, offset, maxResults);
+  public JsonArray getAssignableOpponents(@PathParam("id") Long tournamentId,
+                                          @QueryParam("disciplineId") Long disciplineId,
+                                          @QueryParam("offset") Integer offset,
+                                          @QueryParam("maxResults") Integer maxResults) {
 
-    OpponentTypeRestriction otRestriction = (OpponentTypeRestriction) discipline.getRestriction(OPPONENT_TYPE_RESTRICTION);
+    Discipline discipline = disciplineRepo.read(disciplineId);
+    List<Opponent> opponents = tournamentsRepo.getOpponentsForDiscipline(tournamentId, discipline,
+        offset, maxResults);
+
+    OpponentTypeRestriction otRestriction =
+        (OpponentTypeRestriction) discipline.getRestriction(OPPONENT_TYPE_RESTRICTION);
     OpponentType opponentType = otRestriction.getOpponentType();
-    ModelJsonConverter<Opponent> converter = ocProvider.getConverter(opponentType);
+    ModelJsonConverter<Opponent> converter = opponentConverterProvider.getConverter(opponentType);
 
     return converter.toJsonArray(opponents);
   }
 
   @POST
   @Path("/{tid}/players")
-  public JsonObject addPlayer(@PathParam("tid") Long tournamentId, @QueryParam("pid") Long playerId) {
-    Player player = tRepository.addPlayer(tournamentId, playerId);
-    PlayerJsonConverter pConverter = (PlayerJsonConverter) ocProvider.getConverter(PLAYER);
+  public JsonObject addPlayer(@PathParam("tid") Long tournamentId,
+                              @QueryParam("pid") Long playerId) {
+    Player player = tournamentsRepo.addPlayer(tournamentId, playerId);
+    PlayerJsonConverter playerConverter =
+        (PlayerJsonConverter) opponentConverterProvider.getConverter(PLAYER);
 
-    return pConverter.toJsonObject(player);
+    return playerConverter.toJsonObject(player);
   }
 
   /**
    * Remove player from tournament. The player is not deleted at all.
+   *
    * @param tournamentId ID of the tournament
-   * @param playerId ID of the player to remove
+   * @param playerId     ID of the player to remove
    * @return JsonObject of the removed player
    */
   @DELETE
   @Path("/{tid}/players/{pid}")
-  public JsonObject removePlayer(@PathParam("tid") Long tournamentId, @PathParam("pid") Long playerId) {
-    Player player = tRepository.removePlayer(tournamentId, playerId);
-    PlayerJsonConverter pConverter = (PlayerJsonConverter) ocProvider.getConverter(PLAYER);
+  public JsonObject removePlayer(@PathParam("tid") Long tournamentId,
+                                 @PathParam("pid") Long playerId) {
 
-    return pConverter.toJsonObject(player);
+    Player player = tournamentsRepo.removePlayer(tournamentId, playerId);
+    PlayerJsonConverter playerConverter =
+        (PlayerJsonConverter) opponentConverterProvider.getConverter(PLAYER);
+
+    return playerConverter.toJsonObject(player);
   }
 
   @GET
   @Path("/{id}/players/count")
   public Long playersCount(@PathParam("id") Long tournamentId) {
-    return tRepository.countPlayers(tournamentId);
+    return tournamentsRepo.countPlayers(tournamentId);
   }
 
   @GET
   @Path("/{id}/squads")
-  public JsonArray squads(@PathParam("id") Long tournamentId, @QueryParam("offset") Integer offset, @QueryParam("maxResults") Integer maxResults) {
-    List<Squad> squads = tRepository.getSquads(tournamentId, offset, maxResults);
-    SquadJsonConverter sConverter = (SquadJsonConverter) ocProvider.getConverter(SQUAD);
+  public JsonArray squads(@PathParam("id") Long tournamentId, @QueryParam("offset") Integer offset,
+                          @QueryParam("maxResults") Integer maxResults) {
 
-    return sConverter.toJsonArray(squads);
+    List<Squad> squads = tournamentsRepo.getSquads(tournamentId, offset, maxResults);
+    SquadJsonConverter squadConverter =
+        (SquadJsonConverter) opponentConverterProvider.getConverter(SQUAD);
+
+    return squadConverter.toJsonArray(squads);
   }
 
   @POST
   @Path("/{id}/squads")
   public JsonObject squads(@PathParam("id") Long tournamentId, @QueryParam("sid") Long squadId) {
-    Squad squad = tRepository.addSquad(tournamentId, squadId);
-    SquadJsonConverter sConverter = (SquadJsonConverter) ocProvider.getConverter(SQUAD);
-    
-    return sConverter.toJsonObject(squad);
+    Squad squad = tournamentsRepo.addSquad(tournamentId, squadId);
+    SquadJsonConverter squadConverter =
+        (SquadJsonConverter) opponentConverterProvider.getConverter(SQUAD);
+
+    return squadConverter.toJsonObject(squad);
   }
 
   @GET
   @Path("/{id}/squads/count")
   public Long squadsCount(@PathParam("id") Long tournamentId) {
-    return tRepository.countSquads(tournamentId);
+    return tournamentsRepo.countSquads(tournamentId);
   }
 
   @GET
   @Path("/{id}/disciplines")
   public JsonArray readDisciplines(@PathParam("id") Long tournamentId) {
-    List<Discipline> disciplines = tRepository.getDisciplines(tournamentId, null, null);
+    List<Discipline> disciplines = tournamentsRepo.getDisciplines(tournamentId, null, null);
 
-    return dConverter.toJsonArray(disciplines);
+    return disciplineConverter.toJsonArray(disciplines);
   }
 
   @POST
   @Path("/{id}/disciplines")
-  public JsonObject addDiscipline(@PathParam("id") Long tournamentId, @QueryParam("did") Long disciplineId) {
-    Discipline discipline = dRepository.read(disciplineId);
-    tRepository.addDiscipline(tournamentId, discipline);
+  public JsonObject addDiscipline(@PathParam("id") Long tournamentId,
+                                  @QueryParam("did") Long disciplineId) {
 
-    return dConverter.toJsonObject(discipline);
+    Discipline discipline = disciplineRepo.read(disciplineId);
+    tournamentsRepo.addDiscipline(tournamentId, discipline);
+
+    return disciplineConverter.toJsonObject(discipline);
   }
 
   private void validate(Tournament tournament) {
-      ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-      Validator validator = factory.getValidator();
-      Set<ConstraintViolation<Tournament>> violations = validator.validate( tournament );
-      if(!violations.isEmpty()){
-          throw new ConstraintViolationException(violations);
-      }
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+    Set<ConstraintViolation<Tournament>> violations = validator.validate(tournament);
+    if (!violations.isEmpty()) {
+      throw new ConstraintViolationException(violations);
+    }
   }
 }
