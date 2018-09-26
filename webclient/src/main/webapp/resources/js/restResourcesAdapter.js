20458d9d83c1999890eeb7428b903129d402b1bc
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-09-26 05:59:37 +0200

    remove duplications

--- webclient/src/main/webapp/resources/js/restResourcesAdapter.js
+++ webclient/src/main/webapp/resources/js/restResourcesAdapter.js
@@ -3,8 +3,9 @@
 let restResourceAdapter = {
 
   createOrUpdate: function createOrUpdate(resource, json, onResolve) {
-    restResourceAdapter.clearMessages();
 
+    let url = resourcesUrl() + resource;
+    let body = JSON.stringify(json);
     // if json has an id there is an already persisted instance
     // so using "PUT" for updating the data, otherwise using "POST" for creating
     let method = "PUT";
@@ -12,121 +13,41 @@ let restResourceAdapter = {
       method = "POST";
     }
 
-    fetch(resourcesUrl() + resource, {
+    restResourceAdapter.callRestResource(url, method, body, onResolve);
+  },
+
+  getSingle: function getSingle(resource, id, onResolve) {
+    let url = resourcesUrl() + resource + '/' + id;
+
+    restResourceAdapter.callRestResource(url, "GET", "", onResolve);
+  },
+
+  getMultiple: function getMultiple(resource, offset, maxResults, onResolve) {
+    let url = resourcesUrl() + resource + '?offset=' + offset + '&maxResults=' + maxResults;
+
+    restResourceAdapter.callRestResource(url, "GET", "", onResolve);
+  },
+
+  getMultipleResources: function getMultipleResources(url, offset, maxResults, onResolve) {
+    let completeUrl = url + '?offset=' + offset + '&maxResults=' + maxResults;
+
+    restResourceAdapter.callRestResource(url, "GET", "", onResolve);
+  },
+
+  getResources: function getResources(url, onResolve) {
+    restResourceAdapter.callRestResource(url, "GET", "", onResolve);
+  },
+
+  callRestResource : function callRestResource(url, method, body, onResolve){
+    restResourceAdapter.clearMessages();
+
+    fetch(url, {
       method: method,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(json)
-    })
-      .then(function (response) {
-        // text is a stream and therefore on-time-readable only
-        // https://github.com/whatwg/fetch/issues/196
-        let text = response.text();
-        if (response.ok)
-          text.then(function (respJson) {
-            onResolve(JSON.parse(respJson));
-          })
-        else
-          text.then(function (respJson) {
-            restResourceAdapter.processMessages(JSON.parse(respJson));
-          })
-      })
-      .catch(function (err) {
-        restResourceAdapter.resourceError(err);
-      });
-  },
-
-  getSingle: function getSingle(resource, id, onResolve) {
-    restResourceAdapter.clearMessages();
-
-    fetch(resourcesUrl() + resource + '/' + id, {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then(function (response) {
-        // text is a stream and therefore on-time-readable only
-        // https://github.com/whatwg/fetch/issues/196
-        let text = response.text();
-        if (response.ok)
-          text.then(function (respJson) {
-            onResolve(JSON.parse(respJson));
-          })
-        else
-          text.then(function (respJson) {
-            restResourceAdapter.processMessages(JSON.parse(respJson));
-          })
-      })
-      .catch(function (err) {
-        restResourceAdapter.resourceError(err);
-      });
-  },
-
-  getMultiple: function getMultiple(resource, offset, maxResults, onResolve) {
-    restResourceAdapter.clearMessages();
-
-    fetch(resourcesUrl() + resource + '?offset=' + offset + '&maxResults=' + maxResults, {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then(function (response) {
-        // text is a stream and therefore on-time-readable only
-        // https://github.com/whatwg/fetch/issues/196
-        let text = response.text();
-        if (response.ok)
-          text.then(function (respJson) {
-            onResolve(JSON.parse(respJson));
-          })
-        else
-          text.then(function (respJson) {
-            restResourceAdapter.processMessages(JSON.parse(respJson));
-          })
-      })
-      .catch(function (err) {
-        restResourceAdapter.resourceError(err);
-      });
-  },
-
-  getMultipleResources: function getMultipleResources(url, offset, maxResults, onResolve) {
-    restResourceAdapter.clearMessages();
-
-    fetch(url + '?offset=' + offset + '&maxResults=' + maxResults, {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then(function (response) {
-        // text is a stream and therefore on-time-readable only
-        // https://github.com/whatwg/fetch/issues/196
-        let text = response.text();
-        if (response.ok)
-          text.then(function (respJson) {
-            onResolve(JSON.parse(respJson));
-          })
-        else
-          text.then(function (respJson) {
-            restResourceAdapter.processMessages(JSON.parse(respJson));
-          })
-      })
-      .catch(function (err) {
-        restResourceAdapter.resourceError(err);
-      });
-  },
-
-  getResources: function getResources(url, onResolve) {
-    restResourceAdapter.clearMessages();
-
-    fetch(url, {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json'
-      }
+      body: body
     })
       .then(function (response) {
         // text is a stream and therefore on-time-readable only
