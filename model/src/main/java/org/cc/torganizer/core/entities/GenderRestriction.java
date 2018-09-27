b766c610edc436a97b01ca2b27c832fbce0ef677
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-09-27 06:57:48 +0200

    add final to constant

--- model/src/main/java/org/cc/torganizer/core/entities/GenderRestriction.java
+++ model/src/main/java/org/cc/torganizer/core/entities/GenderRestriction.java
@@ -5,7 +5,7 @@ package org.cc.torganizer.core.entities;
  */
 public class GenderRestriction extends Restriction {
 
-  private static Discriminator DISCRIMINATOR = Discriminator.GENDER_RESTRICTION;
+  private static final Discriminator DISCRIMINATOR = Discriminator.GENDER_RESTRICTION;
 
   private Gender gender = Gender.UNKNOWN;
 
