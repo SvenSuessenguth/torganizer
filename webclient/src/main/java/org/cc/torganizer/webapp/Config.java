6af3dffb28f661a698dd4039432049e12fc6e5d1
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-10-05 08:03:14 +0200

    remove checkstyle warnings

--- webclient/src/main/java/org/cc/torganizer/webapp/Config.java
+++ webclient/src/main/java/org/cc/torganizer/webapp/Config.java
@@ -4,12 +4,19 @@ import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+/**
+ * Bean, in which Configuration is injected and can be used inside XHTML.
+ */
 @RequestScoped
 @Named
-public class Config{
+public class Config {
+  /**
+   * Base-URL for accessing Rest-Services.
+   */
   @Resource(lookup = "torganizer/resourcesUrl")
   private String resourcesUrl;
 
-  public String getResourcesUrl(){
-    return resourcesUrl; }
+  public String getResourcesUrl() {
+    return resourcesUrl;
+  }
 }
