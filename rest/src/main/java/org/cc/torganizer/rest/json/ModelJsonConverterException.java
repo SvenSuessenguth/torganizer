41888509c8749731d4ac760a81b2b1cd14e4267c
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-11-10 08:14:58 +0100

    remove checkstyle warnings

--- rest/src/main/java/org/cc/torganizer/rest/json/ModelJsonConverterException.java
+++ rest/src/main/java/org/cc/torganizer/rest/json/ModelJsonConverterException.java
@@ -1,9 +1,9 @@
 package org.cc.torganizer.rest.json;
 
 public class ModelJsonConverterException extends RuntimeException {
-	private static final long serialVersionUID = 8107389536301181498L;
+  private static final long serialVersionUID = 8107389536301181498L;
 
-	public ModelJsonConverterException(Exception exc) {
-		super(exc);
-	}
+  public ModelJsonConverterException(Exception exc) {
+    super(exc);
+  }
 }
