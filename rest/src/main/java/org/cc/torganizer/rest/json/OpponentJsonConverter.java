f38fa9de515a13bb1dda26a6590bde6bc11449eb
SvenSuessenguth <familie.suessenguth@googlemail.com>
2018-11-11 09:48:31 +0100

    dockerize app

--- rest/src/main/java/org/cc/torganizer/rest/json/OpponentJsonConverter.java
+++ rest/src/main/java/org/cc/torganizer/rest/json/OpponentJsonConverter.java
@@ -4,6 +4,6 @@ import org.cc.torganizer.core.entities.OpponentType;
 
 public interface OpponentJsonConverter {
 
-  public abstract OpponentType getOpponentType();
+  OpponentType getOpponentType();
 
 }
