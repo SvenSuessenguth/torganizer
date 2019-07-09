94c71edad57a368a29d30994f4a722940710f9a3
SvenSuessenguth <familie.suessenguth@googlemail.com>
2019-07-09 20:49:46 +0200

    remove unused code

--- webclient/src/main/webapp/resources/js/crudResource.js
+++ webclient/src/main/webapp/resources/js/crudResource.js
@@ -4,33 +4,6 @@ class CrudResource {
   constructor() {
   }
 
-  createOrUpdate(resource, json, method, onResolve) {
-
-    console.log("create:");
-    console.log(JSON.stringify(json));
-
-    fetch(restResourceAdapter.resourcesUrl()+resource, {
-      method: method,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(json)
-    })
-      .then(function (response) {
-        if (response.ok)
-          return response.json();
-        else
-          throw new Error('errorhandling not specified yet');
-      })
-      .then(function (crudResource) {
-        onResolve(crudResource);
-      })
-      .catch(function (err) {
-        restResourceAdapter.resourceError(err);
-      });
-  }
-
   readSingle(resource, id, onResolve) {
     fetch(restResourceAdapter.resourcesUrl()+resource+'/' + id)
       .then(function (response) {
