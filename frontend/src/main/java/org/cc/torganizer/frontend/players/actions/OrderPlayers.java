d412b21f14cd76573c7194b069a248a24be3b585
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-04-11 17:37:56 +0200

    remove unused imports

--- frontend/src/main/java/org/cc/torganizer/frontend/players/actions/OrderPlayers.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/players/actions/OrderPlayers.java
@@ -2,9 +2,7 @@ package org.cc.torganizer.frontend.players.actions;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.event.AjaxBehaviorEvent;
-import javax.inject.Inject;
 import javax.inject.Named;
-import org.apache.logging.log4j.Logger;
 
 @RequestScoped
 @Named
