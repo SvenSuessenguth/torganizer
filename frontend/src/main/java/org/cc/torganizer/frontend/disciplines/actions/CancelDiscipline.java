5248e7f5658d77d4ae797b2493c68dd09a279458
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-04-26 20:14:46 +0200

    disciplines

--- frontend/src/main/java/org/cc/torganizer/frontend/disciplines/actions/CancelDiscipline.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/disciplines/actions/CancelDiscipline.java
@@ -2,10 +2,6 @@ package org.cc.torganizer.frontend.disciplines.actions;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import org.cc.torganizer.core.entities.AgeRestriction;
-import org.cc.torganizer.core.entities.Discipline;
-import org.cc.torganizer.core.entities.GenderRestriction;
-import org.cc.torganizer.core.entities.OpponentTypeRestriction;
 
 /**
  * Cancel editing the current discipline.
