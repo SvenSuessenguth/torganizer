53b541a3976eedd2cacc68aab67e035076e67a5d
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-08-30 07:16:27 +0200

    update plugins, dependencies

--- frontend/src/main/java/org/cc/torganizer/frontend/clubs/actions/CancelClub.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/clubs/actions/CancelClub.java
@@ -12,7 +12,7 @@ import org.cc.torganizer.core.entities.Club;
 public class CancelClub extends ClubsAction {
 
   /**
-   * creating a club.
+   * creating a new club and replace the club currently under work.
    */
   public String execute() {
     Club club = new Club();
---
+++ model/src/test/java/org/cc/torganizer/core/OpponentToGroupsAssignerFactoryTest.java
@@ -0,0 +1,30 @@
+package org.cc.torganizer.core;
+
+import static java.util.Collections.emptyIterator;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import javax.enterprise.inject.Instance;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class OpponentToGroupsAssignerFactoryTest {
+
+  @Mock
+  private Instance<OpponentToGroupsAssigner> assigners;
+
+  @InjectMocks
+  private OpponentToGroupsAssignerFactory factory;
+
+  @Test
+  void throwMissingOpponentsToGroupsAssignerException() {
+    when(assigners.iterator()).thenReturn(emptyIterator());
+
+    assertThrows(MissingOpponentsToGroupsAssignerException.class, ()
+        -> factory.getOpponentToGroupsAssigner(null));
+  }
+}
