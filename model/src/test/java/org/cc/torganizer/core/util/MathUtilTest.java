8262dcd5ca2a569526a51a9d89fee7cadc0e09cf
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-10-11 12:25:39 +0200

    Tests

---
+++ model/src/test/java/org/cc/torganizer/core/util/MathUtilTest.java
@@ -0,0 +1,27 @@
+package org.cc.torganizer.core.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MathUtilTest {
+
+  @Test
+  void testNextSquare_0() {
+    int actual = MathUtil.nextSquare(0);
+    assertThat(actual).isEqualTo(2);
+  }
+
+  @Test
+  void testNextSquare_127() {
+    int actual = MathUtil.nextSquare(127);
+    assertThat(actual).isEqualTo(128);
+  }
+
+  @Test
+  void testNextSquare_minus1() {
+    int actual = MathUtil.nextSquare(-1);
+    assertThat(actual).isEqualTo(2);
+  }
+}
