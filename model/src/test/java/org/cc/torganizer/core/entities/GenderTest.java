74fbb76f218a1874ade9403ce7dfa03faca8a986
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-10-24 06:55:35 +0200

    raise code coverage

--- model/src/test/java/org/cc/torganizer/core/entities/GenderTest.java
+++ model/src/test/java/org/cc/torganizer/core/entities/GenderTest.java
@@ -1,6 +1,7 @@
 package org.cc.torganizer.core.entities;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.cc.torganizer.core.entities.Gender.MALE;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,26 @@ class GenderTest {
     String name = "UNKNOWN";
     Gender result = Gender.valueOf(name);
     assertThat(result).isNotNull();
+  }
+
+  @Test
+  void testGetId() {
+    String id = MALE.getId();
+
+    assertThat(id).isEqualTo("M");
+  }
+
+  @Test
+  void testById_M() {
+    Gender gender = Gender.byId("M");
+
+    assertThat(gender).isEqualTo(MALE);
+  }
+
+  @Test
+  void testById_NULL() {
+    Gender gender = Gender.byId(null);
+
+    assertThat(gender).isNull();
   }
 }
