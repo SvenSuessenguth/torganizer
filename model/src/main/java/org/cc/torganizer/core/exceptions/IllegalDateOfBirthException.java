767432fc2ba15eaefeab68b9ca60b80e2420567b
SvenSuessenguth <familie.suessenguth@googlemail.com>
2020-12-13 10:22:06 +0100

    Checkstyle

--- model/src/main/java/org/cc/torganizer/core/exceptions/IllegalDateOfBirthException.java
+++ model/src/main/java/org/cc/torganizer/core/exceptions/IllegalDateOfBirthException.java
@@ -1,5 +1,9 @@
 package org.cc.torganizer.core.exceptions;
 
+/**
+ * This exception is thrown, if the DateOfBirth is illegal. The DateOfBirth ist illegal, if the
+ * Person is less than one year old.
+ */
 public class IllegalDateOfBirthException extends RuntimeException {
 
   public IllegalDateOfBirthException(String message) {
