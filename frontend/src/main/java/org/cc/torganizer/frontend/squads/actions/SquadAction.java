fdf208a3cf2a670860416f559d5d9ccf797ce951
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-03-27 10:52:44 +0100

    checkstyle

--- frontend/src/main/java/org/cc/torganizer/frontend/squads/actions/SquadAction.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/squads/actions/SquadAction.java
@@ -4,6 +4,9 @@ import jakarta.inject.Inject;
 import org.cc.torganizer.frontend.ApplicationState;
 import org.cc.torganizer.frontend.squads.SquadsState;
 
+/**
+ * Baseclass for all actions related to squads.
+ */
 public class SquadAction {
 
   @Inject
