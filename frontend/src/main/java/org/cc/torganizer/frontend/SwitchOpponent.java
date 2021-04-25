c455405495c8fe2995985b3eace033537f967c75
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-04-25 10:40:41 +0200

    sonarqube

--- frontend/src/main/java/org/cc/torganizer/frontend/SwitchOpponent.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/SwitchOpponent.java
@@ -2,18 +2,13 @@ package org.cc.torganizer.frontend;
 
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import java.util.Map;
-import org.apache.logging.log4j.Logger;
 
 @Named
 @ViewScoped
 public class SwitchOpponent implements Serializable {
-
-  @Inject
-  private Logger logger;
 
   private Long sourceGroupId;
   private Long sourceOpponentId;
@@ -23,8 +18,6 @@ public class SwitchOpponent implements Serializable {
         getExternalContext().getRequestParameterMap();
     sourceGroupId = Long.valueOf(params.get("gId"));
     sourceOpponentId = Long.valueOf(params.get("oId"));
-
-    logger.info("setSwapSource: " + sourceGroupId + " / " + sourceOpponentId);
   }
 
   public void switchWith() {
@@ -33,6 +26,6 @@ public class SwitchOpponent implements Serializable {
     Long targetGroupId = Long.valueOf(params.get("gId"));
     Long targetOpponentId = Long.valueOf(params.get("oId"));
 
-    logger.info("swapWith: " + sourceGroupId + "/" + sourceOpponentId + " with " + targetGroupId + "/" + targetOpponentId);
+    System.out.println("switch " + sourceGroupId + "/" + sourceOpponentId + " with " + targetGroupId + "/" + targetOpponentId);
   }
 }
