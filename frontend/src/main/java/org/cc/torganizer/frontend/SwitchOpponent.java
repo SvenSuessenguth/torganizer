7c73e0bd0d22f9d046d9a646f98fdd598221004c
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-04-25 10:31:07 +0200

    use logging

--- frontend/src/main/java/org/cc/torganizer/frontend/SwitchOpponent.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/SwitchOpponent.java
@@ -2,13 +2,18 @@ package org.cc.torganizer.frontend;
 
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import java.util.Map;
+import org.apache.logging.log4j.Logger;
 
 @Named
 @ViewScoped
 public class SwitchOpponent implements Serializable {
+
+  @Inject
+  private Logger logger;
 
   private Long sourceGroupId;
   private Long sourceOpponentId;
@@ -19,7 +24,7 @@ public class SwitchOpponent implements Serializable {
     sourceGroupId = Long.valueOf(params.get("gId"));
     sourceOpponentId = Long.valueOf(params.get("oId"));
 
-    System.out.println("setSwapSource: " + sourceGroupId + " / " + sourceOpponentId);
+    logger.info("setSwapSource: " + sourceGroupId + " / " + sourceOpponentId);
   }
 
   public void switchWith() {
@@ -28,6 +33,6 @@ public class SwitchOpponent implements Serializable {
     Long targetGroupId = Long.valueOf(params.get("gId"));
     Long targetOpponentId = Long.valueOf(params.get("oId"));
 
-    System.out.println("swapWith: " + sourceGroupId + "/" + sourceOpponentId + " with " + targetGroupId + "/" + targetOpponentId);
+    logger.info("swapWith: " + sourceGroupId + "/" + sourceOpponentId + " with " + targetGroupId + "/" + targetOpponentId);
   }
 }
