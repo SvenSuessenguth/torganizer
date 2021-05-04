12e9df3e0b46efb9e2d40e4a8632348b599741ef
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-05-04 05:25:24 +0200

    checkstyle

--- frontend/src/main/java/org/cc/torganizer/frontend/disciplines/rounds/actions/AddOpponentToRoundAction.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/disciplines/rounds/actions/AddOpponentToRoundAction.java
@@ -3,13 +3,15 @@ package org.cc.torganizer.frontend.disciplines.rounds.actions;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.cc.torganizer.core.OpponentsToGroupsAssigner;
 import org.cc.torganizer.core.entities.Group;
 import org.cc.torganizer.core.entities.Opponent;
-import org.cc.torganizer.core.entities.PositionalOpponent;
 import org.cc.torganizer.core.entities.Round;
+import org.cc.torganizer.frontend.disciplines.rounds.DisciplineRoundStateSynchronizer;
+import org.cc.torganizer.persistence.RoundsRepository;
 
 /**
  * Adding an opponent to the groups of the round.
@@ -19,7 +21,13 @@ import org.cc.torganizer.core.entities.Round;
 public class AddOpponentToRoundAction extends RoundAction {
 
   @Inject
+  private DisciplineRoundStateSynchronizer synchronizer;
+
+  @Inject
   private OpponentsToGroupsAssigner assigner;
+
+  @Inject
+  private RoundsRepository roundsRepository;
 
   /**
    * Adding an opponent to the groups of the round.
@@ -28,6 +36,11 @@ public class AddOpponentToRoundAction extends RoundAction {
     Round round = roundState.getRound();
     List<Group> groups = round.getGroups();
 
-    assigner.assign(Collections.singleton(opponent), groups);
+    Set<Opponent> opponents = new HashSet<>();
+    opponents.add(opponent);
+    assigner.assign(opponents, groups);
+
+    roundsRepository.update(round);
+    synchronizer.synchronize(roundState);
   }
 }
