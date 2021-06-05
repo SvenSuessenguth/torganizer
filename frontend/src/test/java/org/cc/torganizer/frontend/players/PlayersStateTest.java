3369e7c48e655babc540ae9af4d2c1dab0f2c8cc
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-06-05 09:23:25 +0200

    test PlayersState

--- frontend/src/test/java/org/cc/torganizer/frontend/players/PlayersStateTest.java
+++ frontend/src/test/java/org/cc/torganizer/frontend/players/PlayersStateTest.java
@@ -3,14 +3,19 @@ package org.cc.torganizer.frontend.players;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.cc.torganizer.frontend.players.PlayersState.ALL_PLAYERS_CHUNK_SIZE;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import org.cc.torganizer.core.entities.Club;
 import org.cc.torganizer.core.entities.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PlayersStateTest {
 
@@ -78,5 +83,33 @@ class PlayersStateTest {
     Player player = state.getCurrent();
     Club club = player.getClub();
     assertThat(club).isNull();
+  }
+
+  public static Stream<Arguments> testIsNextAllPlayersChunkAvailableArguments() {
+    return Stream.of(
+        Arguments.of(0, 0, false),
+        Arguments.of(10, 0, false),
+        Arguments.of(11, 0, true),
+        Arguments.of(11, 1, false),
+        Arguments.of(20, 1, false),
+        Arguments.of(21, 1, true)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("testIsNextAllPlayersChunkAvailableArguments")
+  void testIsNextAllPlayersChunkAvailable(int playersSize, int chunkIndex, boolean expected) {
+
+    // prepare list of players
+    List<Player> players = new ArrayList<>();
+    for (int counter = 0; counter < playersSize; counter++) {
+      players.add(new Player());
+    }
+    state.setPlayers(players);
+    state.setAllPlayersChunkIndex(chunkIndex);
+
+    boolean actual = state.isNextAllPlayersChunkAvailable();
+
+    assertThat(actual).isEqualTo(expected);
   }
 }
