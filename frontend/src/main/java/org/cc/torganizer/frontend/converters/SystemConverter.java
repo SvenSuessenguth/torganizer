91921f5e376c6bf1e7bc6b4fc85b3f091ac24102
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-06-26 17:03:01 +0200

    fix errors and test new code

--- frontend/src/main/java/org/cc/torganizer/frontend/converters/SystemConverter.java
+++ frontend/src/main/java/org/cc/torganizer/frontend/converters/SystemConverter.java
@@ -3,6 +3,7 @@ package org.cc.torganizer.frontend.converters;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.ConverterException;
 import jakarta.faces.convert.FacesConverter;
 import org.cc.torganizer.core.entities.System;
 
@@ -14,15 +15,19 @@ public class SystemConverter implements Converter<System> {
 
   @Override
   public System getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
-    if (value.isEmpty()) {
+    if (value == null || value.isEmpty()) {
       return null;
     }
 
-    return System.valueOf(value);
+    try {
+      return System.valueOf(value);
+    } catch (IllegalArgumentException iaExc) {
+      throw new ConverterException(iaExc);
+    }
   }
 
   @Override
   public String getAsString(FacesContext facesContext, UIComponent uiComponent, System system) {
-    return system == null ? "" : "" + system.name();
+    return system == null ? "" : system.name();
   }
 }
