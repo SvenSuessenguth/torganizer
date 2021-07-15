f9a51b2fdd40f892bb4473823bc00654e94f1318
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-07-15 06:41:33 +0200

    add tests

--- model/src/test/java/org/cc/torganizer/core/entities/DisciplineTest.java
+++ model/src/test/java/org/cc/torganizer/core/entities/DisciplineTest.java
@@ -4,6 +4,8 @@ import static java.time.LocalDate.of;
 import static java.time.Month.JANUARY;
 import static java.time.Month.MAY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.cc.torganizer.core.entities.Gender.FEMALE;
 import static org.cc.torganizer.core.entities.Gender.MALE;
 import static org.cc.torganizer.core.entities.OpponentType.PLAYER;
 import static org.cc.torganizer.core.entities.Restriction.Discriminator.AGE_RESTRICTION;
@@ -12,6 +14,8 @@ import static org.cc.torganizer.core.entities.Restriction.Discriminator.OPPONENT
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.cc.torganizer.core.exceptions.RestrictionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -113,5 +117,77 @@ class DisciplineTest {
     Round actual = discipline.getLastRound();
     assertThat(actual).isNotNull();
     assertThat(actual.getPosition()).isEqualTo(2);
+  }
+
+  @Test
+  void addOpponent_restriction() {
+    Player player = new Player("a", "a");
+    player.getPerson().setGender(FEMALE);
+
+    assertThatThrownBy(() -> discipline.addOpponent(player)).isInstanceOf(RestrictionException.class);
+  }
+
+  @Test
+  void addOpponent() {
+    Player player = new Player("a", "a");
+    player.getPerson().setGender(MALE);
+
+    int countBefore = discipline.getOpponents().size();
+    discipline.addOpponent(player);
+    int countAfter = discipline.getOpponents().size();
+
+    assertThat(countBefore + 1).isEqualTo(countAfter);
+  }
+
+  @Test
+  void getPlayers() {
+    Discipline discipline = new Discipline();
+
+    Squad squad = new Squad();
+    squad.addPlayer(new Player());
+    squad.addPlayer(new Player());
+    discipline.addOpponent(squad);
+    discipline.addOpponent(new Player());
+
+    assertThat(discipline.getPlayers()).hasSize(3);
+  }
+
+  @Test
+  void getCurrentRound_noRound() {
+    Optional<Round> currentRound = new Discipline().getCurrentRound();
+    assertThat(currentRound).isEmpty();
+  }
+
+  @Test
+  void getCurrentRound_emptyRound() {
+    Discipline discipline = new Discipline();
+    discipline.addRound(new Round());
+    Optional<Round> currentRound = discipline.getCurrentRound();
+    assertThat(currentRound).isEmpty();
+  }
+
+  @Test
+  void getCurrentRound_emptyGrooup() {
+    Discipline discipline = new Discipline();
+    Round round = new Round();
+    Group group = new Group();
+    round.appendGroup(group);
+    discipline.addRound(round);
+    Optional<Round> currentRound = discipline.getCurrentRound();
+
+    assertThat(currentRound).isEmpty();
+  }
+
+  @Test
+  void getCurrentRound() {
+    Discipline discipline = new Discipline();
+    Round round = new Round();
+    Group group = new Group();
+    group.addOpponent(new Player());
+    round.appendGroup(group);
+    discipline.addRound(round);
+    Optional<Round> currentRound = discipline.getCurrentRound();
+
+    assertThat(currentRound).isPresent();
   }
 }
