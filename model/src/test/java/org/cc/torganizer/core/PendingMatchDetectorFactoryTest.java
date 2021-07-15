3b3afdb4f44fda07793d554e8eb484c07af90607
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-07-15 05:58:00 +0200

    add test bump versions

--- model/src/test/java/org/cc/torganizer/core/PendingMatchDetectorFactoryTest.java
+++ model/src/test/java/org/cc/torganizer/core/PendingMatchDetectorFactoryTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
