e745133f07f9ce45f42436037397cfa0fb23ddae
SvenSuessenguth <familie.suessenguth@googlemail.com>
2021-12-29 09:39:46 +0100

    warnings

--- persistence/src/main/java/org/cc/torganizer/persistence/CreateEntityException.java
+++ persistence/src/main/java/org/cc/torganizer/persistence/CreateEntityException.java
@@ -2,8 +2,12 @@ package org.cc.torganizer.persistence;
 
 import org.cc.torganizer.core.entities.Entity;
 
+/**
+ * Exception on creating (first time persisting via JPA) an entity.
+ */
 public class CreateEntityException extends RuntimeException {
   public CreateEntityException(Entity e, Throwable throwable) {
-    super("Error on creating entity with type '%s'".formatted(e.getClass().getSimpleName()), throwable);
+    super("Error on creating entity with type '%s'".formatted(e.getClass().getSimpleName()),
+        throwable);
   }
 }
