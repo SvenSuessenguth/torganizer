5496c8dff44eccb77488c23195a942610eba9152
SvenSuessenguth <familie.suessenguth@googlemail.com>
2022-01-23 11:55:19 +0100

    cancel rounds

---
+++ frontend/src/main/java/org/cc/torganizer/frontend/disciplines/rounds/actions/CancelRoundAction.java
@@ -0,0 +1,32 @@
+package org.cc.torganizer.frontend.disciplines.rounds.actions;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.cc.torganizer.core.entities.Round;
+import org.cc.torganizer.frontend.disciplines.rounds.DisciplineRoundState;
+import org.cc.torganizer.frontend.disciplines.rounds.DisciplineRoundStateSynchronizer;
+import org.cc.torganizer.persistence.RoundsRepository;
+
+@RequestScoped
+@Named
+public class CancelRoundAction extends RoundAction {
+
+  @Inject
+  private DisciplineRoundState state;
+
+  @Inject
+  private RoundsRepository roundsRepository;
+
+  @Inject
+  private DisciplineRoundStateSynchronizer synchronizer;
+
+  public void execute() {
+    Round round = state.getRound();
+    Long id = round.getId();
+    round = roundsRepository.read(id);
+
+    synchronizer.synchronize(state);
+    state.setRound(round);
+  }
+}
