a8ab0655f4725c791eaac5c5df90b4d3feb502e8
SvenSuessenguth <familie.suessenguth@googlemail.com>
2022-09-09 17:36:58 +0200

    bump versions

--- docker/postgresql/run.bat
+++ docker/postgresql/run.bat
@@ -1,4 +1,4 @@
 docker network create -d bridge carboncopy
-docker pull postgres:14.1
+docker pull postgres:14.5
 docker build -t "carboncopy/postgresql" .
 docker run -d -e POSTGRES_PASSWORD=postgres -p 5432:5432 --name postgresql --network=carboncopy "carboncopy/postgresql":latest
--- persistence/src/test/java/org/cc/torganizer/persistence/AbstractDbUnitJpaTest.java
+++ persistence/src/test/java/org/cc/torganizer/persistence/AbstractDbUnitJpaTest.java
@@ -47,7 +47,7 @@ public abstract class AbstractDbUnitJpaTest {
   // https://stackoverflow.com/questions/69132686/how-can-i-set-the-port-for-postgresql-when-using-testcontainers
   @SuppressWarnings("all")
   @Container
-  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.1")
+  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.5")
       .withDatabaseName("torganizer")
       .withUsername("postgres")
       .withPassword("postgres")
