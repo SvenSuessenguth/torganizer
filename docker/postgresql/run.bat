98c5e24fa0c7efd633355a0c4a839c7e12c9e538
SvenSuessenguth <familie.suessenguth@googlemail.com>
2025-10-05 12:03:18 +0200

    bump postgresql

--- docker/postgresql/run.bat
+++ docker/postgresql/run.bat
@@ -1,4 +1,4 @@
 docker network create -d bridge carboncopy
-docker pull postgres:17.5
+docker pull postgres:18.0
 docker build -t "carboncopy/postgresql" .
 docker run -d -e POSTGRES_PASSWORD=postgres -p 5432:5432 --name postgresql --network=carboncopy "carboncopy/postgresql":latest
